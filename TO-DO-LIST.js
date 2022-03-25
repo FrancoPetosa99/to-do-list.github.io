cd2bdfbf4bf8889a92728f8d2d84db395ff3f9a5
FrancoPetosa99 <franco.petosa15@gmail.com>
2022-03-25 14:56:27 -0300

    toast improved

--- TO-DO-LIST.js
+++ TO-DO-LIST.js
@@ -168,10 +168,10 @@ add_column.addEventListener("click",()=>{
         let message
         if(validator){
             message=`La columna ${input_new_colum.value} ya existe`
-            warning_message(message,modal_new_colum)
+            notification(message,'warning')
         }else if(input_new_colum.value.length==0){
             message="El campo es obligatorio"
-            warning_message(message,modal_new_colum)
+            notification(message,'warning')
         }else if(!validator){
             modal_new_colum.remove()
             adding_column(input_new_colum.value)
@@ -186,6 +186,7 @@ function adding_column(new_colum_name){
     card_box=document.querySelectorAll(".card_box")
     add_option(new_colum_name.toLowerCase())
     colum_state()
+    notification(`Se creo la columna ${new_colum_name}`,'success')
 }
 
 function  add_option(option_to_add){
@@ -265,22 +266,9 @@ function validation(){
         }else if(!tittle_aproved&&!description_aproved){
             message="Ambos campos son obligatorios"
         }
-        const modal=document.querySelector(".modal")
-        warning_message(message,modal)
+        notification(message,'warning')
         return false
     }
-}
-
-function warning_message(message,modal){
-    const tool_tip=document.createElement("p")
-    tool_tip.setAttribute("class","tool_tip")
-    tool_tip.innerText=message
-    modal.appendChild(tool_tip)
-    btn_accept.classList.add("no_events")
-    setTimeout(()=>{
-        modal.removeChild(tool_tip)
-        btn_accept.classList.remove("no_events")
-    },4000)
 }
 
 function render_cards(call_back){
@@ -378,6 +366,7 @@ function add_task(call_back) {
         }
     })
     call_back()
+    notification(`Se creo la tarea ${new_task.tittle}`,'success')
 }
 
 function move_card(card,task,new_state,call_back){
@@ -551,6 +540,38 @@ function delete_column(column,colum_name){
     colum_list=colum_list.filter(element=>element!=colum_name) //ELIMINO DEL ARRAY LA COLUMNA
 }
 
+function notification(message,type){
+    const tool_tip=document.createElement("div")
+    tool_tip.setAttribute("class","tool_tip")
+    const tool_tip_text=document.createElement("p")
+    tool_tip_text.setAttribute("class","tool_tip_text")
+    tool_tip_text.innerText=message
+    const icon=document.createElement("svg")
+    switch (type) {
+        case 'warning':
+            icon.innerHTML=`
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" class="bi bi-exclamation-triangle" viewBox="0 0 16 16">
+                    <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z"/>
+                    <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z"/>
+                </svg>`
+        break;
+        case 'success':
+            icon.innerHTML=`
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" class="bi bi-check-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+                </svg>`
+        break;
+        default:
+        break;
+    }
+    tool_tip.appendChild(icon)
+    tool_tip.appendChild(tool_tip_text)
+    container.appendChild(tool_tip)
+    setTimeout(()=>{
+        container.removeChild(tool_tip)
+    },4000)
+}
 render_columns(render_cards)
  //CUANDO SE TERMINA DE EJECUTAR RENDER_CARDS RECIEN ENTONCES EJECUTA LA FUNCIÓN PASADA COMO CALLBACK COLUMN_STATE
 
